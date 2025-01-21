66ec8d074c448c24771852fd2f3fc4d89e77668c
Hafiz Ahmed Waseem <hafizahmewaseem@gmail.com>
2025-01-21 10:15:42 +0500

    Added new line

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,3 +1,4 @@
-export const IMAGE_BASE_URL=""
+export const IMAGE_BASE_URL = "";
 // export const IMAGE_BASE_URL="https://api.moonerapp.com/"
+// export const IMAGE_BASE_URL="https://api.mooner.com.sg"
 // export const IMAGE_BASE_URL="https://api.mooner.com.sg"
